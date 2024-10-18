2a038f438a11a42633d9508cd0ec35d9d1c8acfb
te04072 <te04072@gmail.com>
2024-10-18 17:59:03 +0900

    accounts templates update

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,11 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = ('username', 'email',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
