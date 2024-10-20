2067c4f2f332ab80ba0da085f73c27d8ba2e5857
gmlgml5023 <gmlgml5023@naver.com>
2024-10-20 18:19:26 +0900

    1) 댓글 목록, 작성, 삭제 기능 구현 // 2) 영화 리뷰 좋아요 기능 구현 // 3) 메인페이지, 상세페이지 수정

--- movies/models.py
+++ movies/models.py
@@ -12,10 +12,17 @@ class Movie(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.pk}. {self.title}'
+
 
 class Comment(models.Model):
     # 영화에 댓글
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     # 사용자의 댓글
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField(max_length=200)
+    content = models.CharField(max_length=200)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
